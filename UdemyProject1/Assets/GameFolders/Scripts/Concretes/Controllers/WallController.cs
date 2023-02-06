5cf5f9883709f72f31132dab30994f3374b8aa00
KckOnur <onur.unique8@gmail.com>
2023-02-06 18:22:02 +0300

    Added WallController

---
+++ UdemyProject1/Assets/GameFolders/Scripts/Concretes/Controllers/WallController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UdemyProject1.Controllers;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+namespace UdemyProejct1.Controllers
+{
+    public class WallController : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision other)
+        {
+            PlayerController player = other.collider.GetComponent<PlayerController>();
+
+            if(player!=null)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+
+    }
+}
+
